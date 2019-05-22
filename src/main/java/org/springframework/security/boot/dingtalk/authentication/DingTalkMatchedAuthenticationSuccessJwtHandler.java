437a892e2cb5e63483799e0c1b4263a4cb73b189
wandl <hnxyhcwdl1003@163.com>
2019-05-22 16:01:10 +0800

    逻辑优化

---
+++ src/main/java/org/springframework/security/boot/dingtalk/authentication/DingTalkMatchedAuthenticationSuccessJwtHandler.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2018, vindell (https://github.com/vindell).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.security.boot.dingtalk.authentication;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationSuccessHandler;
+import org.springframework.security.boot.biz.userdetails.JwtPayloadRepository;
+import org.springframework.security.boot.biz.userdetails.SecurityPrincipal;
+import org.springframework.security.boot.utils.SubjectUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class DingTalkMatchedAuthenticationSuccessJwtHandler implements MatchedAuthenticationSuccessHandler {
+	
+	private JwtPayloadRepository payloadRepository;
+	
+	public DingTalkMatchedAuthenticationSuccessJwtHandler(JwtPayloadRepository payloadRepository) {
+		this.setPayloadRepository(payloadRepository);
+	}
+	
+	@Override
+	public boolean supports(Authentication authentication) {
+		return SubjectUtils.supports(authentication.getClass(), DingTalkAuthenticationToken.class);
+	}
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+    	
+		Map<String, Object> tokenMap = new HashMap<String, Object>();
+		tokenMap.put("code", "0");
+		// 账号首次登陆标记
+		if(SecurityPrincipal.class.isAssignableFrom(userDetails.getClass())) {
+			tokenMap.put("initial", ((SecurityPrincipal) userDetails).isInitial());
+		} else {
+			tokenMap.put("initial", false);
+		}
+		tokenMap.put("perms", userDetails.getAuthorities());
+		tokenMap.put("token", getPayloadRepository().issueJwt((DingTalkAuthenticationToken) authentication));
+		tokenMap.put("username", userDetails.getUsername());
+		
+		response.setStatus(HttpStatus.OK.value());
+		response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+		
+		JSONObject.writeJSONString(response.getWriter(), tokenMap);
+
+	}
+
+	public JwtPayloadRepository getPayloadRepository() {
+		return payloadRepository;
+	}
+
+	public void setPayloadRepository(JwtPayloadRepository payloadRepository) {
+		this.payloadRepository = payloadRepository;
+	}
+
+	
+}
